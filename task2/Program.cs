140f654602d0235022921bc7617db765b75ed005
MaximKharin <maximkharin@gmail.com>
2022-06-16 18:09:52 +0300

    finished task2

---
+++ task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Random number is {num}");
+int firstDigit = num / 100;
+int thirdDigit = num % 10;
+System.Console.WriteLine($"Result is {firstDigit}{thirdDigit}");
